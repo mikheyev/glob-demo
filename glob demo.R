6be748139866bc836b454c191cc7ee83e67fb2cd
Sasha Mikheyev <sasha [at] homologo.us>
2018-11-13 11:23:10 +1100

    basic file parser

--- glob demo.R
+++ glob demo.R
@@ -5,3 +5,11 @@ data.frame(files = Sys.glob("data/*.txt")) %>% separate(files, into = c(NA, "nam
 Sys.glob("data/1*.txt")
 
 Sys.glob("data/??.txt")
+
+
+samples <- tibble(files = Sys.glob("data/*/summary.txt")) %>% separate(files, into = c(NA, "name", NA), sep = "/", remove = F)
+
+summaries <- list()
+for (i in 1:nrow(samples)) {
+  summaries[[i]] <- read_table(as.character(samples[i, "files"]), skip = 3, col_types = "cc-c-")
+}
